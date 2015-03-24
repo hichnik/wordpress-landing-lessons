48f2afa4bb48d8334dfb6d5e152afc762c275384
Iurii Didkovskyi <hichnik@gmail.com>
2015-03-24 02:02:43 +0200

    enough for today

--- js/common.js
+++ js/common.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
-	
-	
+	function setHeight(){
+		$(".main_head").css("height", $(window).height())	
+	}
+
+
+	$(window).resize(function() {
+		setHeight()
+	})
+
+	setHeight()	
 });
+
+
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
+
+
